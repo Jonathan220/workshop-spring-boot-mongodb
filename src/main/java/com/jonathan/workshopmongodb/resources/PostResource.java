0fc81ec2df947f4f145d6d105b5884a6f20a2ce5
jonathan220 <abreu.jonathan220@gmail.com>
2024-09-14 17:08:59 -0300

    Consulta com vários critérios

--- src/main/java/com/jonathan/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/jonathan/workshopmongodb/resources/PostResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,17 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullsearch(@RequestParam(value = "text", defaultValue = "") String text,
+                                                 @RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullsearch(text, min, max);
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
--- src/main/java/com/jonathan/workshopmongodb/services/PostService.java
+++ src/main/java/com/jonathan/workshopmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.jonathan.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +23,12 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullsearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullsearch(text, minDate, maxDate);
+
     }
 
 }
