c10f6978404f6acb095f8f9158197d94f7899987
jonathan220 <abreu.jonathan220@gmail.com>
2024-09-14 15:58:59 -0300

    Consulta simples com query methods

--- src/main/java/com/jonathan/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/jonathan/workshopmongodb/resources/PostResource.java
@@ -1,11 +1,14 @@
 package com.jonathan.workshopmongodb.resources;
 
 import com.jonathan.workshopmongodb.domain.Post;
+import com.jonathan.workshopmongodb.resources.util.URL;
 import com.jonathan.workshopmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -14,11 +17,11 @@ public class PostResource {
     @Autowired
     private PostService postService;
 
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Post> findById(@PathVariable String id){
-        Post obj = postService.findById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(obj);
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
 }
